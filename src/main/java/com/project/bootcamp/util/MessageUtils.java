06240f2f5c01f45226f786353b20d9c4cc824cfd
ismair <ismair@gmail.com>
2021-06-13 10:17:23 -0300

    initial commit

---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,8 @@
+package com.project.bootcamp.util;
+
+public abstract class MessageUtils {
+
+    public static final String STOCK_ALREADY_EXISTS = "STROCK ALREADY EXISTS IN THE DATABASE";
+    public static final String NO_RECORD_FOUND = "NO RECORD FOUND";
+
+}
